b797bdc7e5a7c6ecd626002f8a0adcbba2b85f4a
luthfibphoenix <lutfibahtiar2409@gmail.com>
2024-12-29 08:16:28 +0700

    membuat PengelolaHalaman

---
+++ app/src/main/java/com/example/pertemuan12/ui/Navigasi/PengelolaHalaman.kt
@@ -0,0 +1,38 @@
+package com.example.pertemuan12.ui.Navigasi
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.example.pertemuan12.ui.View.DestinasiEntry
+import com.example.pertemuan12.ui.View.DestinasiHome
+import com.example.pertemuan12.ui.View.EntryMhsScreen
+import com.example.pertemuan12.ui.View.HomeScreen
+
+@Composable
+fun PengelolaHalaman(navController: NavHostController = rememberNavController()){
+    NavHost(
+        navController = navController,
+        startDestination = DestinasiHome.route,
+        modifier = Modifier,
+    ){
+        composable(DestinasiHome.route){
+            HomeScreen(
+                navigateToItemEntry = {navController.navigate(DestinasiEntry.route)},
+                onDetailClick = {
+
+                }
+            )
+        }
+        composable(DestinasiEntry.route){
+            EntryMhsScreen(navigateBack = {
+                navController.navigate(DestinasiHome.route) {
+                    popUpTo(DestinasiHome.route) { inclusive = true }
+                }
+            }
+            )
+        }
+    }
+}
