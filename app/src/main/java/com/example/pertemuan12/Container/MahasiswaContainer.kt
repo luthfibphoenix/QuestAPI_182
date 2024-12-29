107fcefb95d614fb0be67170dddae810589c88ee
luthfibphoenix <lutfibahtiar2409@gmail.com>
2024-12-29 07:10:56 +0700

    membuat class MahasiswaContainer dan membuat properti

--- app/src/main/java/com/example/pertemuan12/Container/MahasiswaContainer.kt
+++ app/src/main/java/com/example/pertemuan12/Container/MahasiswaContainer.kt
@@ -1,6 +1,12 @@
 package com.example.pertemuan12.Container
 
 import com.example.pertemuan12.repository.MahasiswaRepository
+import com.example.pertemuan12.repository.NetworkKontakRepository
+import com.example.pertemuan12.service.MahasiswaService
+import com.jakewharton.retrofit2.converter.kotlinx.serialization.asConverterFactory
+import kotlinx.serialization.json.Json
+import okhttp3.MediaType.Companion.toMediaType
+import retrofit2.Retrofit
 
 interface AppContainer{
     val kontakRepository: MahasiswaRepository
@@ -8,5 +14,17 @@ interface AppContainer{
 
 class MahasiswaContainer: AppContainer{
 
+    private val baseUrl = "https:10.0.2.2:800/umyTI/"
+    private val json = Json{ignoreUnknownKeys = true}
+    private val retrofit: Retrofit = Retrofit.Builder()
+        .addConverterFactory(json.asConverterFactory("application/json".toMediaType()))
+        .baseUrl(baseUrl).build()
 
+    private val mahasiswaService: MahasiswaService by lazy{
+        retrofit.create(MahasiswaService::class.java)
+    }
+
+    override val kontakRepository: MahasiswaRepository by lazy{
+        NetworkKontakRepository(mahasiswaService)
+    }
 }
