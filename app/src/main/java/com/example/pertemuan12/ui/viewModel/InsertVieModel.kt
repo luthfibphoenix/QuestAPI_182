d485356b94965c6dd3d112a28c3153359b377a68
luthfibphoenix <lutfibahtiar2409@gmail.com>
2024-12-29 07:22:59 +0700

    membuat fungsi InsertUiEvent.toMahasiswa

--- app/src/main/java/com/example/pertemuan12/ui/viewModel/InsertVieModel.kt
+++ app/src/main/java/com/example/pertemuan12/ui/viewModel/InsertVieModel.kt
@@ -3,6 +3,14 @@ package com.example.pertemuan12.ui.viewModel
 import com.example.pertemuan12.Model.Mahasiswa
 
 
+fun InsertUiEvent.toMahasiswa(): Mahasiswa = Mahasiswa(
+    nim = nim,
+    nama = nama,
+    alamat = alamat,
+    jenisKelamin = jenisKelamin,
+    kelas = kelas,
+)
+
 fun Mahasiswa.toInsertUiEvent():InsertUiEvent = InsertUiEvent{
     nim = nim,
     nama = nama,
