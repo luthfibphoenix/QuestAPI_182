0053b6b373561b68c8b83e628dd6a644a33fafe9
luthfibphoenix <lutfibahtiar2409@gmail.com>
2024-12-29 07:52:13 +0700

    Membuat file HomeView

---
+++ app/src/main/java/com/example/pertemuan12/ui/View/HomeView.kt
@@ -0,0 +1,9 @@
+package com.example.pertemuan12.ui.View
+
+import com.example.pertemuan12.ui.Navigasi.DestinasiNavigasi
+
+object DestinasiHome: DestinasiNavigasi {
+    override val route ="home"
+    override val titleRes = "Home Mhs"
+}
+
