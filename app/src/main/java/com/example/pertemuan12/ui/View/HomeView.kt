5141ca062ee63640d7075f052c49649be697c4b4
luthfibphoenix <lutfibahtiar2409@gmail.com>
2024-12-30 13:07:05 +0700

    mengubah judul

--- app/src/main/java/com/example/pertemuan12/ui/View/HomeView.kt
+++ app/src/main/java/com/example/pertemuan12/ui/View/HomeView.kt
@@ -46,7 +46,7 @@ import com.example.pertemuan12.ui.viewModel.PenyediaViewModel
 
 object DestinasiHome: DestinasiNavigasi {
     override val route ="home"
-    override val titleRes = "Home Mhs"
+    override val titleRes = "Home Mahasiswa"
 }
 
 @OptIn(ExperimentalMaterial3Api::class)
