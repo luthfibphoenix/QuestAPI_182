a2e3d0a653d55b7b73c1dfd346b586b03d2c08c3
luthfibphoenix <lutfibahtiar2409@gmail.com>
2024-12-30 08:03:53 +0700

    membuat fungsi ItemDetailMahasiswa

--- app/src/main/java/com/example/pertemuan12/ui/View/DetailView.kt
+++ app/src/main/java/com/example/pertemuan12/ui/View/DetailView.kt
@@ -1,16 +1,47 @@
 package com.example.pertemuan12.ui.View
 
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Card
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.input.pointer.PointerIcon.Companion.Text
+import androidx.compose.ui.unit.dp
+import com.example.pertemuan12.Model.Mahasiswa
+
+@Composable
+fun ItemDetailMahasiswa(
+    modifier: Modifier = Modifier,
+    mahaasiswa: Mahasiswa
+){
+    Card(
+        modifier = modifier.fillMaxWidth()
+    ){
+        Column(
+            modifier = modifier.padding(16.dp)
+        ){
+            ComponentDetailMahasiswa(judul = "NIM", isinya = mahaasiswa.nim)
+            Spacer(modifier = Modifier.padding(5.dp))
+            ComponentDetailMahasiswa(judul = "Nama", isinya = mahaasiswa.nama)
+            Spacer(modifier = Modifier.padding(5.dp))
+            ComponentDetailMahasiswa(judul = "Alamat", isinya = mahaasiswa.alamat)
+            Spacer(modifier = Modifier.padding(5.dp))
+            ComponentDetailMahasiswa(judul = "Jenis Kelamin", isinya = mahaasiswa.jenisKelamin)
+            Spacer(modifier = Modifier.padding(5.dp))
+            ComponentDetailMahasiswa(judul = "Kelas", isinya = mahaasiswa.kelas)
+            Spacer(modifier = Modifier.padding(5.dp))
+            ComponentDetailMahasiswa(judul = "Angkatan", isinya = mahaasiswa.angkatan)
+        }
+    }
+}
 
 @Composable
 fun ComponentDetailMahasiswa(
-    modifier: Modifier,
+    modifier: Modifier = Modifier,
     judul: String,
     isinya: String
 ){
