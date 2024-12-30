c0b9268ef93b1bdad2839640b441fdb6dbc34b90
luthfibphoenix <lutfibahtiar2409@gmail.com>
2024-12-30 09:06:52 +0700

    menambahkan rute untuk detail dan update

--- app/src/main/java/com/example/pertemuan12/ui/Navigasi/PengelolaHalaman.kt
+++ app/src/main/java/com/example/pertemuan12/ui/Navigasi/PengelolaHalaman.kt
@@ -8,10 +8,15 @@ import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
 import androidx.navigation.navArgument
+import com.example.pertemuan12.ui.View.DestinasiDetail
 import com.example.pertemuan12.ui.View.DestinasiEntry
 import com.example.pertemuan12.ui.View.DestinasiHome
+import com.example.pertemuan12.ui.View.DestinasiUpdate
+import com.example.pertemuan12.ui.View.DetailScreen
 import com.example.pertemuan12.ui.View.EntryMhsScreen
 import com.example.pertemuan12.ui.View.HomeScreen
+import com.example.pertemuan12.ui.View.UpdateScreen
+import com.example.pertemuan12.ui.viewModel.UpdateViewModel
 
 @Composable
 fun PengelolaHalaman(navController: NavHostController = rememberNavController()){
@@ -24,15 +29,13 @@ fun PengelolaHalaman(navController: NavHostController = rememberNavController())
             HomeScreen(
                 navigateToItemEntry = {navController.navigate(DestinasiEntry.route)},
                 onDetailClick = {nim ->
-                    navController.navigate("${DestinasiEntry.route}/$nim"){
-                        popUpTo(DestinasiHome.route) {
-                            inclusive = true
-                        }
-                    }
+                    navController.navigate("${DestinasiEntry.route}/$nim")
                     println("PengelolaHalaman: nim = $nim")
                 }
             )
         }
+
+
         composable(DestinasiEntry.route){
             EntryMhsScreen(navigateBack = {
                 navController.navigate(DestinasiHome.route) {
@@ -41,6 +44,24 @@ fun PengelolaHalaman(navController: NavHostController = rememberNavController())
                     }
                 }
             })
+        }
+
+        composable(DestinasiDetail.routeWithArg) { backStackEntry ->
+            val nim = backStackEntry.arguments?.getString(DestinasiDetail.NIM) ?: ""
+            DetailScreen(
+                navigateBack = { navController.navigateUp() },
+                onEditClick = {
+                    navController.navigate("${DestinasiUpdate.route}/$nim")
+                }
+            )
+        }
+
+        composable(DestinasiUpdate.routeWithArg){ backStackEntry ->
+            val nim = backStackEntry.arguments?.getString(DestinasiUpdate.NIM) ?: ""
+            UpdateScreen(
+                navigateBack = { navController.navigateUp() },
+                onNavigate = { navController.navigate(DestinasiHome.route) }
+            )
         }
     }
 }
